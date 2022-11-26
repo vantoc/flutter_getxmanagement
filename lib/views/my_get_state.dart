3e5627a3140ac9193dee4c74064a9d55c0343cbb
vantoc <mayt3156@gmail.com>
2022-11-27 00:24:00 +0700

    Reactive State Manager GetX(by using Obx)

--- lib/views/my_get_state.dart
+++ lib/views/my_get_state.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+// void main() {
+//   runApp(MyGetSateManagement());
+// }
 
 class MyGetSateManagement extends StatelessWidget {
-  const MyGetSateManagement({super.key});
+  var count = 0.obs;
+
+  void increment() {
+    count++;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+      title: "State Management",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("State Management"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Obx(
+                () => Text(
+                  'Count value is $count',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              ElevatedButton(
+                child: Text('Increment'),
+                onPressed: () {
+                  increment();
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
