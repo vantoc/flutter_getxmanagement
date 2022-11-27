ab0182ad18b7055a85047f407fe113a535dfbcd0
vantoc <mayt3156@gmail.com>
2022-11-28 00:08:39 +0700

    Dependency Injection Get.put - Get.lazyPut - Get.putAsync - Get.create

---
+++ lib/views/dependency_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/dependency_injection_controller.dart';
+
+class DependencyInjectionPage extends StatelessWidget {
+  DependencyInjectController myController =
+      Get.put(DependencyInjectController(), tag: 'instancel', permanent: true);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Dependency injection",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Dependency injection"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    //// instance will created with tag
+                    Get.find<DependencyInjectController>()(tag: 'instance');
+
+                    Get.find<DependencyInjectController>();
+
+                    Get.find<DependencyInjectController>().incrementCounter;
+                  },
+                  child: Text('Click Me'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
